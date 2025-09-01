aaa08162bc420bccedd2ef477f6341077c755a1b
x0rw <mahdi.svt5@gmail.com>
2025-09-01 12:48:36 +0100

    initial test func

---
+++ main.py
@@ -0,0 +1,21 @@
+import logging
+import json
+import azure.functions as func
+
+app = func.FunctionApp()
+
+
+@app.function_name(name="eventGridTrigger")
+@app.event_grid_trigger(arg_name="event")
+def eventGridTest(event: func.EventGridEvent):
+    result = json.dumps(
+        {
+            "id": event.id,
+            "data": event.get_json(),
+            "topic": event.topic,
+            "subject": event.subject,
+            "event_type": event.event_type,
+        }
+    )
+
+    logging.info("Python EventGrid trigger processed an event: %s", result)
